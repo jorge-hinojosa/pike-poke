0acf4b0a478100d3180cea33fa1873225e334248
Jorge Hinojosa <jorge94@live.com>
2019-08-22 09:48:12 -0500

    added mq to menu

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-const path = require("path");
 require("dotenv").config();
+const path = require("path");
 const express = require("express");
 const massive = require("massive");
 const app = express();
